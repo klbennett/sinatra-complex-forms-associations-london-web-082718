8609e3a9ef4a09e6afbccb5bb6229e3ea5d804e4
Kay B <kaylouisebennett@gmail.com>
2018-09-22 19:24:14 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -8,7 +8,7 @@ class OwnersController < ApplicationController
 #use erb to get all of the pets with Pet.all
 #iterate over collection of objects and generate checkbox for each pets
 # checkbox has a name of "owner[pet_ids][]" because we want to structure our params hash such that the array of pet IDs is stored inside the "owner" hash
-
+#checkbox has a value of given pets ID, so when a checkbox is selected it's value gets sent through to params hash
   get '/owners/new' do
     @pets = Pet.all
     erb :'/owners/new'
