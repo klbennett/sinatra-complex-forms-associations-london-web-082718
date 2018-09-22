0186a5f422c48ded4b91f961311015e83a611e19
Kay B <kaylouisebennett@gmail.com>
2018-09-22 19:48:06 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,9 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
+
+  # That checkbox has a name of "owner[pet_ids][]" because we want to structure our params hash such that the array of pet IDs is stored inside the "owner" hash. We are aiming to associate the pets that have these IDs with the new owner.
+  # We give the checkbox a value of the given pet's ID. This way, when that checkbox is selected, its value, i.e., the pet's ID, is what gets sent through to the params hash.
   post '/owners' do
   @owner = Owner.create(params[:owner])
   if !params["pet"]["name"].empty? #need this condition to avoid creating pet with empty name if user leaves box blank
